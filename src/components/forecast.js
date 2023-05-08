1f103c19b7b5dfa2c07682e26be10e111426b9b2
OlhaDorofiychuk <olha.d@yahoo.com>
2023-05-08 16:42:26 +0100

    forecast-days started

--- src/components/forecast.js
+++ src/components/forecast.js
@@ -1,14 +1,22 @@
 import { Accordion, AccordionItem, AccordionItemHeading,AccordionItemButton, AccordionItemPanel } from "react-accessible-accordion"
 
-
+const weekDays=['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 export const Forecast =({data})=>{
+    const dayOfTheWeek = new Date().getDay()
+    const forecastWeek= weekDays.slice(dayOfTheWeek, weekDays.length).concat(weekDays.slice(0,dayOfTheWeek))
+    console.log(forecastWeek)
     return (
     <>
     <label className="title">Daily</label>
     <Accordion allowMultipleExpanded>
         {data.list.splice(0,7).map((item,index)=>(
             <AccordionItem key={index}>
-            <AccordionItemHeading><AccordionItemButton>'day'
+            <AccordionItemHeading><AccordionItemButton>
+                <div className="daily-item">
+                    <img alt="weather" className="icon-small" src={`icons/${item.weather[0].icon}.png`} />
+                    <label className="day"></label>
+
+                </div>
                 </AccordionItemButton></AccordionItemHeading>
             <AccordionItemPanel></AccordionItemPanel>
             </AccordionItem>
