eec800605e082e310aec10ce0f5be7cd150308e6
OlhaDorofiychuk <olha.d@yahoo.com>
2023-05-08 16:59:40 +0100

    title description added forecast

--- src/components/forecast.js
+++ src/components/forecast.js
@@ -14,7 +14,9 @@ export const Forecast =({data})=>{
             <AccordionItemHeading><AccordionItemButton>
                 <div className="daily-item">
                     <img alt="weather" className="icon-small" src={`icons/${item.weather[0].icon}.png`} />
-                    <label className="day"></label>
+                    <label className="day">{forecastWeek[index]}</label>
+                    <label className="description"> {item.weather[0].description}</label>
+                    <label className="min-max"> {Math.round(item.main.temp_min)}°C / {Math.round(item.main.temp_max)}°C</label>
 
                 </div>
                 </AccordionItemButton></AccordionItemHeading>
